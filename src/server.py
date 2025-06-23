c1017e39adace1fd3dc3818a780cb920cf4b4d28
LucBerge <lucas.bergeron@outlook.fr>
2025-06-23 14:33:13 +0200

    `/find` is now asynchronous

--- src/server.py
+++ src/server.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, jsonify
 import combinaison
 from mailmeteor import MailMeteor
+from threading import Thread
+import requests
 
 app = Flask(__name__)
 mailmeteor = MailMeteor()
@@ -14,23 +16,34 @@ def find():
     domain = data.get('domain')
     if not domain:
         return jsonify({'error': 'domain is required'}), 400
-    
-    print(f"Finding emails for {name} at {domain}...")
+    callback = data.get('callback')
+    if not callback:
+        return jsonify({'error': 'callback is required'}), 400
 
-    valid = invalid = []
-    emails = combinaison.generate_email_combinations(name, domain)
-    for email in emails:
-        print(f"Checking email: {email} ({emails.index(email) + 1}/{len(emails)})")
-        is_valid = mailmeteor.verify_email(email)
-        if is_valid:
-            valid.append(email)
-        else:
-            invalid.append(email)
+    def process_find(name, domain, callback):
+        print(f"Finding emails for {name} at {domain}...")
 
-    return jsonify({
-        'valid': valid,
-        'invalid': invalid
-    })
+        valid = invalid = []
+        emails = combinaison.generate_email_combinations(name, domain)
+        for idx, email in enumerate(emails):
+            print(f"Checking email: {email} ({idx + 1}/{len(emails)})")
+            is_valid = mailmeteor.verify_email(email)
+            if is_valid:
+                valid.append(email)
+            else:
+                invalid.append(email)
+        
+        result = {
+            'valid': valid,
+            'invalid': invalid
+        }
+
+        print(f"Finished finding emails for {name} at {domain}. Valid: {len(valid)}, Invalid: {len(invalid)}")
+        print(f"Sending results to callback URL: {callback}")
+        requests.post(callback, json=result)
+
+    Thread(target=process_find, args=(name, domain, callback)).start()
+    return '', 200
 
 @app.route('/check', methods=['POST'])
 def check():
@@ -42,6 +55,7 @@ def check():
     print(f"Checking email: {email}")
 
     is_valid = mailmeteor.verify_email(email)
+    print(f"Email {email} is {'valid' if is_valid else 'invalid'}")
     return jsonify({'valid': is_valid})
 
 if __name__ == '__main__':
