87598fd76f732b2731e0811145653ed6361deab0
LucBerge <lucas.bergeron@outlook.fr>
2025-07-08 20:20:51 +0200

    Update log message

--- src/email_finder.py
+++ src/email_finder.py
@@ -13,7 +13,7 @@ class EmailFinder:
         verifier = EmailVerifierFactory.create(self.verifier_type)
         try:
             if verifier.verify_email(f"this_is_a_random_email@{domain}"):
-                raise ValueError(f"Domain {domain} is not compatible with {self.verifier_type}: The answer is always True.")
+                raise ValueError(f'Domain {domain} is not compatible with "{self.verifier_type}" verifier: The answer is always True.')
         finally:
             verifier.close()
 
