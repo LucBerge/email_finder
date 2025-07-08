1689ea4aab52dec89319601994826bd2c4f3326d
LucBerge <lucas.bergeron@outlook.fr>
2025-07-08 20:20:46 +0200

    Handle errors

--- src/server.py
+++ src/server.py
@@ -12,49 +12,55 @@ PORT = os.getenv('PORT', None)
 
 @app.route('/find', methods=['POST'])
 def find():
-    data = request.get_json()
-    name = data.get('name')
-    if not name:
-        return jsonify({'error': 'name is required'}), 400
-    domain = data.get('domain')
-    if not domain:
-        return jsonify({'error': 'domain is required'}), 400
-    verifier = data.get('verifier')
-    if not verifier:
-        return jsonify({'error': 'verifier is required'}), 400
-    
-    callback = data.get('callback')
-    # If not callback provided, blocking call, return results directly
-    if not callback:
-        emails = EmailFinder(verifier).find_email(name, domain)
-        return jsonify(emails), 200
-    # If callback provided, run in a separate thread, return results later to callback
-    else:
-        def process_find(name, domain, callback):
+    try:
+        data = request.get_json()
+        name = data.get('name')
+        if not name:
+            return jsonify({'error': 'name is required'}), 400
+        domain = data.get('domain')
+        if not domain:
+            return jsonify({'error': 'domain is required'}), 400
+        verifier = data.get('verifier')
+        if not verifier:
+            return jsonify({'error': 'verifier is required'}), 400
+        
+        callback = data.get('callback')
+        # If not callback provided, blocking call, return results directly
+        if not callback:
             emails = EmailFinder(verifier).find_email(name, domain)
-            print(f"Sending results to callback URL: {callback}")
-            requests.post(callback, json=emails)
+            return jsonify(emails), 200
+        # If callback provided, run in a separate thread, return results later to callback
+        else:
+            def process_find(name, domain, callback):
+                emails = EmailFinder(verifier).find_email(name, domain)
+                print(f"Sending results to callback URL: {callback}")
+                requests.post(callback, json=emails)
 
-        Thread(target=process_find, args=(name, domain, callback)).start()
-        return '', 200
+            Thread(target=process_find, args=(name, domain, callback)).start()
+            return '', 200
+    except Exception as e:
+        return jsonify({'error': str(e)}), 400
 
 @app.route('/check', methods=['POST'])
 def check():
-    data = request.get_json()
-    email = data.get('email')
-    if not email:
-        return jsonify({'error': 'email is required'}), 400
-    verifier = data.get('verifier')
-    if not verifier:
-        return jsonify({'error': 'verifier is required'}), 400
+    try:
+        data = request.get_json()
+        email = data.get('email')
+        if not email:
+            return jsonify({'error': 'email is required'}), 400
+        verifier = data.get('verifier')
+        if not verifier:
+            return jsonify({'error': 'verifier is required'}), 400
 
-    print(f"Checking email: {email}")
+        print(f"Checking email: {email}")
 
-    verifier = EmailVerifierFactory.create(verifier)
-    is_valid = verifier.verify_email(email)
-    verifier.close()
-    print(f"Email {email} is {'valid' if is_valid else 'invalid'}")
-    return jsonify({'valid': is_valid})
+        verifier = EmailVerifierFactory.create(verifier)
+        is_valid = verifier.verify_email(email)
+        verifier.close()
+        print(f"Email {email} is {'valid' if is_valid else 'invalid'}")
+        return jsonify({'valid': is_valid})
+    except Exception as e:
+        return jsonify({'error': str(e)}), 400
 
 if __name__ == '__main__':
     app.run(debug=False, host=HOST, port=PORT)
