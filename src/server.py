1d2019d2942b883f0cb8c47233e485392056ee91
LucBerge <lucas.bergeron@outlook.fr>
2025-07-08 19:44:50 +0200

    Blocking call if no callback provided, add verifier param

--- src/server.py
+++ src/server.py
@@ -9,7 +9,6 @@ app = Flask(__name__)
 
 HOST = os.getenv('HOST', 'localhost')
 PORT = os.getenv('PORT', None)
-EMAIL_VERIFIER = os.getenv('EMAIL_VERIFIER', 'mailmeteor')
 
 @app.route('/find', methods=['POST'])
 def find():
@@ -20,19 +19,24 @@ def find():
     domain = data.get('domain')
     if not domain:
         return jsonify({'error': 'domain is required'}), 400
+    verifier = data.get('verifier')
+    if not verifier:
+        return jsonify({'error': 'verifier is required'}), 400
+    
     callback = data.get('callback')
+    # If not callback provided, blocking call, return results directly
     if not callback:
-        return jsonify({'error': 'callback is required'}), 400
+        emails = EmailFinder(verifier).find_email(name, domain)
+        return jsonify(emails), 200
+    # If callback provided, run in a separate thread, return results later to callback
+    else:
+        def process_find(name, domain, callback):
+            emails = EmailFinder(verifier).find_email(name, domain)
+            print(f"Sending results to callback URL: {callback}")
+            requests.post(callback, json=emails)
 
-    def process_find(name, domain, callback):
-        print(f"Finding emails for {name} at {domain}...")
-        emails = EmailFinder(EMAIL_VERIFIER).find_email(name, domain)
-        print(f"Finished finding emails for {name} at {domain}")
-        print(f"Sending results to callback URL: {callback}")
-        requests.post(callback, json=emails)
-
-    Thread(target=process_find, args=(name, domain, callback)).start()
-    return '', 200
+        Thread(target=process_find, args=(name, domain, callback)).start()
+        return '', 200
 
 @app.route('/check', methods=['POST'])
 def check():
@@ -40,10 +44,13 @@ def check():
     email = data.get('email')
     if not email:
         return jsonify({'error': 'email is required'}), 400
+    verifier = data.get('verifier')
+    if not verifier:
+        return jsonify({'error': 'verifier is required'}), 400
 
     print(f"Checking email: {email}")
 
-    verifier = EmailVerifierFactory.create(EMAIL_VERIFIER)
+    verifier = EmailVerifierFactory.create(verifier)
     is_valid = verifier.verify_email(email)
     verifier.close()
     print(f"Email {email} is {'valid' if is_valid else 'invalid'}")
